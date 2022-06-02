aaf03204feadc3c6108f2d00f74daeecf2cafcb1
Meklit <meklitfrehun12@gmail.com>
2022-06-02 02:10:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,4 @@
 #include<stdio.h>
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
-}
-#include<stdio.h>
-/**
-* main - Entry point
-*
-* Return always 0 
-*/
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
-#include<stdio.h>
 /**
 * main - Entry point
 *
