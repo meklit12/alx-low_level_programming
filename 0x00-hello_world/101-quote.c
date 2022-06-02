5da6734e89cf26595b6592eb1c510a40274fea81
Meklit <meklitfrehun12@gmail.com>
2022-06-02 06:32:52 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
- #include<stdio.h>
+
+#include <unistd.h>
 /**
-* main - Entry point
+* main - Entry-point
+* Return: Always 1 (Success)
 *
-* Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-return (1); } 
+char strl[] = " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2,strl,59);
+return (1);
+} 
