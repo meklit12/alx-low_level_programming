49941e5e0590525a0a0b203e0f4c7b4564154096
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-06-14 09:08:35 +0300

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,21 @@
 #include "main.h"
+
 /**
- *_strcpy- copies string
- *@dest:checked
- *@src: checked
- *Return: dest
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-
-
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	int i;
+	int index = 0;
 
-	i = 0;
-
-	while (src[i])
+	while (src[index])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
 
 	return (dest);
