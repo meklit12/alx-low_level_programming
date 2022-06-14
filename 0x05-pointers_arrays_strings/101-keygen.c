ca7928a3e2152882617fe8c26d199bd729f13e5d
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-06-14 09:18:02 +0300

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - generates keygen.
- * Return: 0 Always.
+
+/** 
+ *main- genrate password
+ *Return:nothing
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+srand((unsigned int)time(NULL));
+char pass[13];
+int i;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+
+for (i = 0; i < 4; i++)
+{
+
+pass[3 * i] = '0' + (rand() % 10);
+char capLetter = 'A' + (rand() % 26);
+
+pass[(3 * i) + 1] = capLetter;
+char letter = 'a' + (rand() % 26);
+
+pass[(3 * i) + 2] = letter;
+}
+pass[3 * i] = '\0';
+printf("generated password : %s\n\n", pass);
+
+printf("\n\n");
+return (0);
 }
