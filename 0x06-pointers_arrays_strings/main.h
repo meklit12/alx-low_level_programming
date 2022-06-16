da4b1b1fc98cd0a52430f5b6ed610a4a9edc1462
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-06-16 10:18:19 +0300

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -7,4 +10,9 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-int _putchar(char c);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
