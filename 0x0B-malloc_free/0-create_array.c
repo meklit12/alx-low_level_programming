23069d4f39c2335a16fc7427cffd00b5c7f34ab2
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-06-27 15:54:33 +0300

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,30 @@
-#include "main.h"
-/**
- * malloc_checked - allocates memory using malloc
- * @b: unsigned int of amount of needed memory
- * Return: void
- */
-void *malloc_checked(unsigned int b)
-{
-	void *p;
+#include "holberton.h"
+#include <stdlib.h>
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	return (p);
+/**
+ *create_array- creats array
+ *@c: checked
+ *@size: size of the array
+ *Return: array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
 }
