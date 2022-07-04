668cdef8632608ec50594c2fb21b7163c168d0a9
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-07-04 10:47:37 +0300

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,7 +2,6 @@
 #define OBJECT_LIKE_MACRO_H
 /*
  * File: 0-object_like_macro.h
- * Auth: Brennan D Baraban
  * Desc: Header file that defines a macro named
  *       SIZE as an abbreviation for the token 1024.
  */
