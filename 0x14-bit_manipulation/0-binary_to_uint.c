6506bcd579214f563ddf2afa5e1d7057a5921717
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-07-21 14:00:40 +0300

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,64 +1,28 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
- * binary_to_uint - converts binary num to uint
- * Return: uint
- * @b: binary number as string
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * Prototype: unsigned int binary_to_uint(const char *b);
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * -> there is one or more chars in the string b that is not 0 or 1
+ * -> b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec, size, i, exp;
+	unsigned int value = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	size = _strlen(b);
-	i = 0;
-	dec = 0;
-	exp = size - 1;
-
-	while (b[i])
+	while (*b != '\0')
 	{
-		if (b[i] == '1')
-			dec += power(2, exp);
-		else if (b[i] != '0')
+		value = value << 1;
+		if (*b != '1' && *b != '0')
 			return (0);
-		exp--;
-		i++;
+		else if (*b == '1')
+			value = value | 1;
+		b++;
 	}
-	return (dec);
-}
-/**
- * _strlen - len of string.
- * @s: is a pointer to a char
- * Return: len.
- */
-
-unsigned int _strlen(const char *s)
-{
-	unsigned int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * power - power of base
- * @base: base
- * @exp: exponent
- * Return: base raised to exp
- */
-unsigned int power(unsigned int base, unsigned int exp)
-{
-	unsigned int i = 0;
-	unsigned int raised = 1;
-
-
-	if (exp == 0)
-		return (1);
-
-	for (i = 0; i < exp; i++)
-		raised = raised * base;
-	return (raised);
+	return (value);
 }
